b0efb32c228e2943fd2166ef4330d5d54138601b
José Omar Gaitán <jose.gaitan@usp.ceu.es>
2025-10-01 13:21:44 +0000

    mas cambios

--- ejercicio 1/figuras.py
+++ ejercicio 1/figuras.py
@@ -1,19 +1,24 @@
 class Figura:
-    def __init__(self, color: str):
+    def __init__(self, color):
         self.color = color
-    def __str__(self) -> str:
-        return f'Color = "{self.color}"'
 
 class Circulo(Figura):
-    def __init__(self, color: str, diametro: float):
+    def __init__(self, color, diametro):
         super().__init__(color)
         self.diametro = diametro
-    def __str__(self) -> str:
-        return f'{super().__str__()}\nDiámetro = {self.diametro}'
 
-class Cuadrado(Figura):
-    def __init__(self, color: str, longitud: float):
+class Rectangulo(Figura):
+    def __init__(self, color, longitud, anchura):
         super().__init__(color)
         self.longitud = longitud
-    def __str__(self) -> str:
-        return f'{super().__str__()}\nLongitud = {self.longitud}'
+        self.anchura = anchura
+
+class Cuadrado(Rectangulo):  # El cuadrado ES un rectángulo especial
+    def __init__(self, color, lado):
+        super().__init__(color, lado, lado)
+
+class Elipse(Figura):
+    def __init__(self, color, eje_mayor, eje_menor):
+        super().__init__(color)
+        self.eje_mayor = eje_mayor
+        self.eje_menor = eje_menor
---
+++ ejercicio 1/iteracionesfig.py
@@ -0,0 +1,9 @@
+from figuras import Figura, Circulo, Rectangulo, Cuadrado, Elipse
+
+Circulo = Circulo("negro", 1)
+Rectangulo = Rectangulo("Naranja", 3, 1)
+Cuadrado = Cuadrado("Azul", 1.5)
+Elipse = Elipse("Amarilla", 3, 1)
+
+
+
